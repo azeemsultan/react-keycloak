f3104d1034c83294f4c08469ad875f0bfb02f71f
azeemsultan <43080738+azeemsultan@users.noreply.github.com>
2021-04-13 19:05:03 +0500

    React-Keycloak - First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Welcome from './Welcome';
+import Secured from './Secured';
 import './App.css';
+import { Divider, Grid, Paper, Typography } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          
+       
+            <Grid container style={{marginTop:'50px'}}>
+              <Grid item md={2}></Grid>
+
+              <Grid item md={8} xs={12}>
+                <Paper elevation={3} > 
+                  <Typography variant="h5" style={{paddingTop:'20px',textAlign:'center'}}>
+                    Welcome to Secured login app
+                  </Typography>
+                  <Divider/>
+                  <Typography style={{textAlign:'center',marginTop:'20px'}}>Please login to proceed</Typography>
+                  <div style={{width:'100%',textAlign:'center',paddingBottom:'50px'}}>
+                  <Link to="/secured">Click here to login</Link>
+                      <br/>
+                      <br/>
+                      <Divider/>
+                      <br/>
+                       <Typography>Home page</Typography>
+                       <Link to="/">public component</Link>
+                  </div>
+                </Paper>
+              </Grid>
+
+              <Grid item md={2}></Grid>
+            </Grid>
+        
+          
+          <Route exact path="/" component={Welcome} />
+          <Route path="/secured" component={Secured} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
 export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="Welcome">
+        <p style={{textAlign:'center'}}>This is your home page and anyone can see this page. you dont need to login to view this page</p>
+      </div>
+    );
+  }
+}
+export default Welcome;
